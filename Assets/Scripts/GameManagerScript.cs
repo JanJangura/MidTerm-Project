ad302eae85b50e93eb69d6b15812e7353a2c81a2
NotTylee <Markodurden2004@gmail.com>
2022-11-01 16:55:00 -0400

    I forgot

--- Assets/Scripts/GameManagerScript.cs
+++ Assets/Scripts/GameManagerScript.cs
@@ -7,26 +7,7 @@ public class GameManagerScript : MonoBehaviour
 {
     public GameObject gameOverUI;
     // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.visible = false;
-        Cursor.lockState = CursorLockMode.Locked;   
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (gameOverUI.activeInHierarchy)
-        {
-            Cursor.visible = true;
-            Cursor.lockState = CursorLockMode.None;
-        }
-        else
-        {
-            Cursor.visible = false;
-            Cursor.lockState = CursorLockMode.Locked;
-        }
-    }
+    
     public void gameOver()
     {
         gameOverUI.SetActive(true);
